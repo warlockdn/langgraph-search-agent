bab4908c629a1f1bc2e2f6188300df1261ba91b2
Deepankar <warlockdn@gmail.com>
2026-03-15 17:40:50 +0530

    fix: update graph.py

--- src/agent_search/graph.py
+++ src/agent_search/graph.py
@@ -20,17 +20,10 @@ def build_graph(config: AppConfig | None = None, retriever: Any | None = None):
     builder.add_node("prepare_tool_input", nodes.prepare_tool_input)
     builder.add_node("initial_tool_choice", nodes.initial_tool_choice)
     builder.add_node("call_tool", nodes.call_tool)
-    builder.add_node("start_agent_search", nodes.start_agent_search)
-    builder.add_node("generate_sub_answers_subgraph", nodes.generate_sub_answers_subgraph)
-    builder.add_node("retrieve_orig_question_docs_subgraph_wrapper", nodes.retrieve_orig_question_docs_subgraph_wrapper)
-    builder.add_node("generate_initial_answer", nodes.generate_initial_answer)
+    builder.add_node("run_initial_research_agent", nodes.run_initial_research_agent)
     builder.add_node("validate_initial_answer", nodes.validate_initial_answer)
-    builder.add_node("extract_entity_term", nodes.extract_entity_term)
     builder.add_node("decide_refinement_need", nodes.decide_refinement_need)
-    builder.add_node("create_refined_sub_questions", nodes.create_refined_sub_questions)
-    builder.add_node("answer_refined_question_subgraphs", nodes.answer_refined_question_subgraphs)
-    builder.add_node("ingest_refined_sub_answers", nodes.ingest_refined_sub_answers)
-    builder.add_node("generate_validate_refined_answer", nodes.generate_validate_refined_answer)
+    builder.add_node("run_refinement_research_agent", nodes.run_refinement_research_agent)
     builder.add_node("compare_answers", nodes.compare_answers)
     builder.add_node("logging_node", nodes.logging_node)
 
@@ -42,35 +35,24 @@ def build_graph(config: AppConfig | None = None, retriever: Any | None = None):
         nodes.route_after_initial_choice,
         {
             "call_tool": "call_tool",
-            "start_agent_search": "start_agent_search",
+            "run_initial_research_agent": "run_initial_research_agent",
         },
     )
 
     builder.add_edge("call_tool", "logging_node")
-
-    builder.add_edge("start_agent_search", "generate_sub_answers_subgraph")
-    builder.add_edge("start_agent_search", "retrieve_orig_question_docs_subgraph_wrapper")
-    builder.add_edge("generate_sub_answers_subgraph", "generate_initial_answer")
-    builder.add_edge("retrieve_orig_question_docs_subgraph_wrapper", "generate_initial_answer")
-
-    builder.add_edge("generate_initial_answer", "validate_initial_answer")
-    builder.add_edge("validate_initial_answer", "extract_entity_term")
-    builder.add_edge("extract_entity_term", "decide_refinement_need")
+    builder.add_edge("run_initial_research_agent", "validate_initial_answer")
+    builder.add_edge("validate_initial_answer", "decide_refinement_need")
 
     builder.add_conditional_edges(
         "decide_refinement_need",
         nodes.route_after_refinement_decision,
         {
-            "create_refined_sub_questions": "create_refined_sub_questions",
+            "run_refinement_research_agent": "run_refinement_research_agent",
             "compare_answers": "compare_answers",
         },
     )
 
-    builder.add_edge("create_refined_sub_questions", "answer_refined_question_subgraphs")
-    builder.add_edge("answer_refined_question_subgraphs", "ingest_refined_sub_answers")
-    builder.add_edge("ingest_refined_sub_answers", "generate_validate_refined_answer")
-    builder.add_edge("generate_validate_refined_answer", "compare_answers")
-
+    builder.add_edge("run_refinement_research_agent", "compare_answers")
     builder.add_edge("compare_answers", "logging_node")
     builder.add_edge("logging_node", END)
 
